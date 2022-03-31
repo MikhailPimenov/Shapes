cf8c44ef9529844fc59240bc40ace1a59117c9f9
MikhailPimenov <misha4git@yandex.ru>
2022-03-31 18:13:51 +0300

    develop/first commit. .gitignore, Shapes.cpp are added

---
+++ Shapes.cpp
@@ -0,0 +1,29 @@
+// Shapes.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+/*
+Необходимо привести пример программы или библиотеки на C++, 
+которая выводит на экран различные геометрические фигуры: 
+круг, квадрат, треугольник, прямоугольник и т.п. 
+Глубина проработки примера (количество фигур, параметры, методы) 
+на Ваше усмотрение. Программа не обязательно должна запускаться 
+и работать (хотя это будет плюсом).
+*/
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
