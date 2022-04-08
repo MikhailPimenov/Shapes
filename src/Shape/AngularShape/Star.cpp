fc84da3f4c56b2f2efa72b61925e020e61bed5a7
MikhailPimenov <misha4git@yandex.ru>
2022-04-08 16:45:55 +0300

    develop/made angle constants in Star.cpp static

--- src/Shape/AngularShape/Star.cpp
+++ src/Shape/AngularShape/Star.cpp
@@ -1,9 +1,8 @@
 #include "Star.h"
 //18 90 162 234 306
 
-
-const double pi = std::acos(-1.0);
-const double pi_devided_by_180 = pi / 180.0;
+static const double pi = std::acos(-1.0);
+static const double pi_devided_by_180 = pi / 180.0;
 
 static const Star_angles_t g_angles{ 
 	pi_devided_by_180 * (360.0 - 90.0), 
@@ -13,15 +12,6 @@ static const Star_angles_t g_angles{
 	pi_devided_by_180 * (360.0 - 234.0) 
 };
 
-
-Star::Star() : AngularShape{ {
-		Point(std::cos(g_angles[0ull]),  std::sin(g_angles[0ull])),
-		Point(std::cos(g_angles[1ull]),  std::sin(g_angles[1ull])),
-		Point(std::cos(g_angles[2ull]),  std::sin(g_angles[2ull])),
-		Point(std::cos(g_angles[3ull]),  std::sin(g_angles[3ull])),
-		Point(std::cos(g_angles[4ull]),  std::sin(g_angles[4ull]))
-} } {
-}
 
 Star::Star(const Point& center, double radius) : AngularShape{ {
 		Point(
